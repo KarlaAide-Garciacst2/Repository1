e07527b6877db87d85532d1b02d37bddb772486b
Universidad Politecnica de Gómez Palacio <upgop@iMac-de-Universidad-2.local>
2019-01-21 09:12:51 -0600

    Repository1

---
+++ Repository1/Repository1Tests/Repository1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Repository1Tests.swift
+//  Repository1Tests
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 19-01-21.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import XCTest
+@testable import Repository1
+
+class Repository1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
